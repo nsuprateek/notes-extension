a44237b31d7eb00c4e1505e2a4cfb9a535ca9032
Raghav Thakar <raghavthakar12@gmail.com>
2023-10-21 20:37:10 -0700

    rename popup.html to notes.html

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 const chatBox = document.getElementById('chat-box');
 const messageInput = document.getElementById('message-input');
 const clearMessagesButton = document.getElementById('clear-messages'); // Get the button element
+const postMessageButton = document.getElementById('post-message'); // Get the button element
 
 // Function to scroll to the bottom of the chat box
 function scrollToBottom() {
@@ -9,34 +10,48 @@ function scrollToBottom() {
 
 // Function to add a message to the chat box
 function addMessageToChat(message, timestamp, url) {
-  chatBox.innerHTML += `<p><strong>${timestamp} (${url}):</strong> ${message}</p>`;
+  chatBox.innerHTML += `<p><strong>${timestamp}<br></strong> <i>(${url})<br></i>n${message}</p><br>`;
   messageInput.value = '';
   scrollToBottom();
+}
+
+function postMessage(message) {
+  if (message) {
+    const timestamp = new Date().toLocaleTimeString();
+  //   addMessageToChat(message, timestamp, ''); // Displaying an empty URL for now
+
+    // Capture the active tab's URL
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const url = tabs[0] ? tabs[0].url : 'N/A';
+      addMessageToChat(message, timestamp, url);
+
+      // Send the message, timestamp, and URL to the service worker to store
+      chrome.runtime.sendMessage({ action: 'addMessage', message, timestamp, url });
+    });
+  }
 }
 
 // Event listener for typing and sending a message
 messageInput.addEventListener('keyup', function (event) {
   if (event.key === 'Enter') {
     const message = messageInput.value;
-    if (message) {
-      const timestamp = new Date().toLocaleTimeString();
-    //   addMessageToChat(message, timestamp, ''); // Displaying an empty URL for now
-
-      // Capture the active tab's URL
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        const url = tabs[0] ? tabs[0].url : 'N/A';
-        addMessageToChat(message, timestamp, url);
-
-        // Send the message, timestamp, and URL to the service worker to store
-        chrome.runtime.sendMessage({ action: 'addMessage', message, timestamp, url });
-      });
-    }
+    postMessage(message);
   }
 });
 
+postMessageButton.addEventListener('click', function () {
+    const message = messageInput.value;
+    postMessage(message);
+  });
+
 // Event listener for clicking the "Clear Messages" button
 clearMessagesButton.addEventListener('click', function () {
-    clearMessages();
+    // Use a confirmation prompt
+    const confirmClear = window.confirm("Are you sure you want to clear all messages?");
+    
+    if (confirmClear) {
+      clearMessages();
+    }
   });
   
   // Function to clear all messages
